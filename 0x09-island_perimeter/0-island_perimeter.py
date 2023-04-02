e9659b0dd834b7af4173e47a97a627f8ba6c8dae
alli-cpp <alli-cpp@github.com>
2023-04-02 21:43:55 +0000

    another

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-
-""" returns perimeter of the island in grid for"""
-
+""" returns the perimeter of the island described in grid """
 
 
 def island_perimeter(grid):
-    """ returns the perimeter of island described in grid """
+    """ returns the perimeter of the island described in grid """
     rows = len(grid)
+    cols = len(grid[0])
     perimeter = 0
     for i in range(rows):
-        for j in range(len(grid[i])):
+        for j in range(cols):
             if grid[i][j] == 1:
                 perimeter += 4
                 if i > 0 and grid[i-1][j] == 1:
                     perimeter -= 2
-                    if j > 0 and grid[i][j-1] == 1:
-                        perimeter -= 2
-                        
-                        
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
     return perimeter
