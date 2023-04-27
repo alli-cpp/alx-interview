ca6e3d16ff6e05c3d54401d03042225fd67abbd8
alli-cpp <alli-cpp@github.com>
2023-04-27 04:41:37 -0700

    operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
 """
-0-minoperations.py
-      
+0-minoperations
+
 """
 
 
-def minOperations(n):
-    if n <= 1:
-      return 0
-    i = 2
-    operations = 0
-    while i * i <= n:
-      while n % i == 0:
-        operations += i
-        n //= i
-        i += 1
-      if n > 1:
-         operations += n
-    return operations
+def minOperations(n) -> int:
+    """A method to compute the min operations needed to result H characters"""
+    if type(n) != int:
+        return 0
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    while done < n:
+        if clipboard == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif n - done > 0 and (n - done) % done == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif clipboard > 0:
+            done += clipboard
+            ops_count += 1
+    return ops_count
