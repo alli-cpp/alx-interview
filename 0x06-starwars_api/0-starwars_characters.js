addc8e5fefc31174fa23aabade0d7ababb4b650b
alli-cpp <alli-cpp@github.com>
2023-04-27 05:55:56 -0700

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
+
 const request = require('request');
+
 const movieId = process.argv[2];
+
 const options = {
   url: 'https://swapi-api.hbtn.io/api/films/' + movieId,
   method: 'GET'
